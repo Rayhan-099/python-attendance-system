2fa9e7329ba770c3d4be953dc117005fdde77b53
Rayhan099 <khanrayhan8307@gmail.com>
2026-02-16 20:31:19 +0530

    libraries

---
+++ app.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import ttk, messagebox
+import cv2
+import os
+import csv
+import numpy as np
+from PIL import Image, ImageTk
+import pandas as pd
+import datetime
+import time
+import hashlib
+from pathlib import Path
